f933be0a203a5f2e59caf21fca2c6c456d1aa771
Quinn McHugh <qpmchugh@outlook.com>
2019-12-04 22:41:23 -0500

    Added features

--- rpi-book-reader/__main__.py
+++ rpi-book-reader/__main__.py
@@ -1,18 +1,57 @@
-"""Module startup code for Raspberry Pi book reader.
+"""Module defining code to setup the book reader's GUI.
 
 Uses Google Python Style Guide: https://google.github.io/styleguide/pyguide.html
 """
 
-from camera import *
-from gui import *
-from motor import *
+#from camera import *
+from configparser import ConfigParser
+#from motor import *
+from reader import *
+import os
 
-motor = Motor(15, 16, (17, 18, 19)) # Change these pins
-camera = Camera()
-gui = GUI()
-gui.run()
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.behaviors import ToggleButtonBehavior
+from kivy.uix.behaviors import ButtonBehavior
+from kivy.uix.button import Button
+from kivy.uix.camera import Camera
+from kivy.uix.image import Image
+from kivy.uix.label import Label
+from kivy.uix.progressbar import ProgressBar
+from kivy.uix.tabbedpanel import TabbedPanel
 
-import pyttsx
-engine = pyttsx.init()
-engine.say('Good morning.')
-engine.runAndWait()
+Builder.load_file("__main__.kv")
+
+class RPIBookReader(App, TabbedPanel):
+
+    def build(self):
+        parser = ConfigParser()
+        parser.read(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'config.ini'))
+
+        #self.motor = Motor(15, 16, (17, 18, 19)) # Change these pins
+        #self.camera = Camera()
+        self.reader = Reader(parser.get('TesseractSettings', 'PathToTesseractEXE'))
+        
+        return self
+
+class ImageButton(ButtonBehavior, Image):
+    """Represents an Image that behaves like a button."""
+
+class ToggleButtonImage(ToggleButtonBehavior, Image):
+    """Represents an Image that behaves like a toggle button."""
+
+    def toggle_state(self, widget, value):
+        """Toggles the play/pause button"""
+        app = App.get_running_app()
+        if (value == "down"):
+            self.source = "./icons/pause-button.png"
+            app.reader.play()
+        else:
+            self.source = "./icons/play-button.png"
+            app.reader.pause()
+
+if __name__ == "__main__":
+    app = RPIBookReader()
+    app.run()
--- rpi-book-reader/reader.py
+++ rpi-book-reader/reader.py
@@ -3,26 +3,19 @@
 Uses Google Python Style Guide: https://google.github.io/styleguide/pyguide.html
 """
 
-from enum import enum
-import pyttsx3
 from gtts import gTTS
-from enum import enum
-engine = pyttsx3.init()
-engine.say('The quick brown fox jumped over the lazy dog.')
-engine.runAndWait()
-
-engine = pyttsx3.init()
-voices = engine.getProperty('voices')
-for voice in voices:
-   engine.setProperty('voice', voice.id)  # changes the voice
-   engine.say('The quick brown fox jumped over the lazy dog.')
-engine.runAndWait()
+from enum import Enum
+import os
+from PIL import Image
+from playsound import playsound
+from pytesseract import *
 
 class Reader:
-    """Reads aloud text according to the user's preferences.
+    """Reads aloud text from camera according to the user's preferences.
 
     Attributes:
         voice: The voice engine used to speak words aloud.
+        paused: Whether or not the reader is paused.
     """
 
     class Voice(Enum):
@@ -30,11 +23,44 @@ class Reader:
         GOOGLE = 1
         PYTTSX = 2
 
-    def __init__(self, voice):
+    def __init__(self, pathToTesseractEXE=None, voice=None):
         """Initializes Reader class"""
-        if voice is not None:
-            self.voice = Voice.GOOGLE
+        if voice is None:
+            self.voice = Reader.Voice.GOOGLE
+        else:
+            self.voice = voice
+
+        self.paused = True
+
+        if pathToTesseractEXE is not None:
+            pytesseract.tesseract_cmd = pathToTesseractEXE
 
     def change_voice(self, voice):
         """Changes the reader's voice to the specified voice"""
-        if voice = Voice.Google
+        try:
+            self.voice = voice;
+        except ValueError:
+            raise Exception(voice + "is not a valid voice type." )
+
+    def pause(self):
+        """Pauses the reader"""
+        self.paused = True
+
+    def play(self):
+        """Plays the reader."""
+        self.paused = False
+
+    def read_image(self, image_file):
+        string = self.__image_to_string(image_file)
+        self.__read(string)
+
+    def __image_to_string(self, image_file):
+        return pytesseract.image_to_string(Image.open(image_file))
+
+    def __read(self, string):
+        if (self.voice == Reader.Voice.GOOGLE):
+            print ("reading")
+            tts = gTTS(text='Good morning', lang='en')
+            audio_file_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'audio', 'temp.mp3')
+            tts.save(audio_file_path)
+            playsound(audio_file_path)
